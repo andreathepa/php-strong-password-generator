e4df3e6c5b63a8c5cb09e6e6e5fddeb58f598580
andreathepa <andreadepa32@gmail.com>
2023-07-06 09:37:36 +0200

    milestone 2 done

--- index.php
+++ index.php
@@ -1,23 +1,7 @@
 <?php
 include __DIR__ . "/partials/template/function.php";
 
-function randomPassword() {
-    if (isset($_GET['password'])) {
-        $passlength = intval($_GET['password']);
-        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-        $pass = array();
-        $alphaLength = strlen($alphabet) - 1;
 
-        for ($i = 0; $i < $passlength; $i++) {
-            $n = rand(0, $alphaLength);
-            $pass[] = $alphabet[$n];
-        }
-
-        return implode($pass);
-    }
-
-    return ''; // Ritorna una stringa vuota se il parametro password non è stato inviato
-}
 ?>
 
 <!DOCTYPE html>
--- partials/template/function.php
+++ partials/template/function.php
@@ -1,6 +1,21 @@
 <?php
 
-    $ciao = 'ciao'
+function randomPassword() {
+    if (isset($_GET['password'])) {
+        $passlength = intval($_GET['password']);
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array();
+        $alphaLength = strlen($alphabet) - 1;
 
+        for ($i = 0; $i < $passlength; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+
+        return implode($pass);
+    }
+
+    return ''; // Ritorna una stringa vuota se il parametro password non è stato inviato
+}
 
 ?>
